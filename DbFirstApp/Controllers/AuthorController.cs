be5cfffeb957b06fccd736d1edf759c0d4362ba6
shrirampanchal <shriram_panchal@yahoo.com>
2021-02-26 14:59:15 +0530

    Add project files.

---
+++ DbFirstApp/Controllers/AuthorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DbFirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DbFirstApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController : ControllerBase
+    {
+       
+        
+        [HttpGet]
+        public IEnumerable<Author> Get()
+        {
+           using (var context = new BookStoresDbContext())
+            {
+                return context.Authors.ToList();
+                //return context.Authors.Where(any => any.AuthorId == 1).ToList();
+            }           
+        }
+    }
+}
